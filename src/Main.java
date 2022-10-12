347f18c87cc7c65c63a899aaab567989e97a2fbd
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2022-10-12 22:27:58 +0300

    change main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double k = 1_000_000; // сумма кредита
-        double y = 3; // количество лет кредита
+        double y = 1; // количество лет кредита
         double b = 9.99; // процентная ставка
-        double payment = service.payment(k, y, b);
+        double payment = service.payment(1_000_000, 1, 9.99);
+        System.out.println(payment);
+
+        payment = service.payment(1_000_000, 2, 9.99);
+        System.out.println(payment);
+
+        payment = service.payment(1_000_000, 3, 9.99);
         System.out.println(payment);
     }
 }
